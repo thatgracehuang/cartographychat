dccf7157432e1fcdbbcd6f59ad71088fa2122c50
Grace <gh971@nyu.edu>
2018-05-27 16:39:56 +0400

    from socket.io

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-// server side
-
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  // res.send('<h1>Hello world</h1>');
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
   socket.on('chat message', function(msg){
-    // console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
