5c12cd92f4a4fd605fbae4c0340edbd0f0931912
Grace <gh971@nyu.edu>
2018-05-27 16:21:51 +0400

    trying something out

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   // res.send('<h1>Hello world</h1>');
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + 'public');
 });
 
 io.on('connection', function(socket){
